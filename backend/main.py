6b607004b38cbacf546f984df5ee46849091c7c2
surajrastogi1 <surajrastogi606@gmail.com>
2026-07-07 13:55:11 +0530

    login and jwt authentication

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,23 @@
-from fastapi import FastAPI,HTTPException,Depends
+from fastapi import FastAPI,HTTPException,Depends,status
 from sqlmodel import SQLModel,create_engine,Field,Session,select
 from pydantic import EmailStr
+from datetime import datetime,timedelta,timezone
 import bcrypt
+import jwt
+
+SECRET_KEY = "my_super_secret_key_for_researchmind_ai"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def create_access_tokens(data : dict):
+    to_encode = data.copy()
+
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp" : expire})
+
+    encoded_jwt = jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
+    return encoded_jwt
+
 
 def get_password_hash(password: str) -> str:
     # Convert plain text to bytes, salt it, and hash it
@@ -10,6 +26,11 @@ def get_password_hash(password: str) -> str:
     hashed = bcrypt.hashpw(pwd_bytes, salt)
     return hashed.decode('utf-8') # Convert back to a clean text string for the DB
 
+def verify_password(plain_password : str , hashed_password : str) -> bool:
+    return bcrypt.checkpw(
+        plain_password.encode("utf-8"),
+        hashed_password.encode("utf-8")
+    )
 
 
 class User(SQLModel,table=True):
@@ -23,6 +44,9 @@ class UserCreate(SQLModel): #this helps to user enter these things
     email: EmailStr
     password: str
 
+class UserLogin(SQLModel):
+    username : str
+    password : str
 
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
@@ -72,6 +96,29 @@ def register(user_data : UserCreate, session : Session = Depends(get_session)):
     session.refresh(new_user)
 
     return {"message" : "User Registered Successfully!" , "user_id" : new_user.id}
+
+@app.post("/login")
+def login(login_data : UserLogin, session : Session = Depends(get_session)):
+    user = session.exec(select(User).where(User.username == login_data.username)).first()
+
+    if not user:
+        raise HTTPException(
+            status_code= status.HTTP_404_UNAUTHORIZED,
+            detail= "Invalid username or password"
+        )
+    elif not verify_password(login_data.password,user.hashed_password):
+        raise HTTPException(
+            status_code= 401,
+            detail= "Invalid username or password"
+        )
+    
+    access_token = create_access_tokens(data = {"sub" : user.username})
+    
+    return {
+        "access_token" : access_token,
+        "token_type" : "bearer",
+        "username" : user.username
+    }
 
 
 
